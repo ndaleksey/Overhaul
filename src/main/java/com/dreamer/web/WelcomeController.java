8bb370c3aeaca2b4fa9ea5f7f44d421cb17f6063
Alex <ugtemp@mail.ru>
2014-09-18 23:18:16 +0400

    Initial commit

---
+++ src/main/java/com/dreamer/web/WelcomeController.java
@@ -0,0 +1,52 @@
+package com.dreamer.web;
+
+        import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Shishkov A.V. on 10.09.2014.
+ */
+@Controller
+@RequestMapping(value = "/")
+public class WelcomeController {
+
+//    @Autowired
+//    private QuestionService questionService;
+
+    @RequestMapping(value = "/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/examples")
+    public String examples() {
+        return "examples";
+    }
+
+    @RequestMapping(value = "/order_request")
+    public String order_request() {
+        return "order_request";
+    }
+
+    @RequestMapping(value = "/order_response", method = RequestMethod.POST)
+    public String order_response(@RequestParam(required = true, defaultValue = "Гость") String userName,
+                                @RequestParam(required = true) String email,
+                                @RequestParam(required = true) String address,
+                                @RequestParam(required = true) String phoneNumber,
+                                @RequestParam(required = true, defaultValue = "0.0") float flatSquare,
+                                @RequestParam(required = true, defaultValue = "1") int roomsQuantity,
+                                @RequestParam String message, ModelMap model) {
+
+        model.addAttribute("userName", userName);
+        model.addAttribute("email", email);
+        model.addAttribute("address", address);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("flatSquare", flatSquare);
+        model.addAttribute("roomsQuantity", roomsQuantity);
+
+        return "order_response";
+    }
+}
